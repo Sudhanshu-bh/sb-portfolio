4fef9eff8cc85c99bc4f0553c928061ce81b79ac
Sudhanshu <bsudhanshu3@gmail.com>
2024-02-25 17:05:47 +0530

    logo changes

--- components/Header.js
+++ components/Header.js
@@ -3,6 +3,7 @@ import Image from 'next/image';
 
 // next link
 import Link from 'next/link';
+import Logo from '../public/logo.svg';
 
 // components
 import Socials from '../components/Socials';
@@ -13,14 +14,11 @@ const Header = () => {
 			<div className="container mx-auto">
 				<div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
 					{/* logo */}
-					<Link href={'/'}>
-						<Image
-							src={'/logo.svg'}
-							width={220}
-							height={48}
-							alt=""
-							priority={true}
-						/>
+					<Link href={'/'} className="lg:hidden">
+						<Image src={Logo} width={280} height={38} alt="" priority={true} />
+					</Link>
+					<Link href={'/'} className="hidden lg:flex">
+						<Image src={Logo} width={350} height={48} alt="" priority={true} />
 					</Link>
 					{/* socials */}
 					<Socials />
