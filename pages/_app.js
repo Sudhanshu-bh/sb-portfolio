e97afeccb95197643616ab49c7aea35f8c079e9a
Sudhanshu <bsudhanshu3@gmail.com>
2024-03-03 11:41:47 +0530

    metadata added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import '../styles/globals.css';
 
+// next head
+import Head from 'next/head';
+
 // components
 import Layout from '../components/Layout';
 import Transition from '../components/Transition';
@@ -13,14 +16,20 @@ import { AnimatePresence, motion } from 'framer-motion';
 function MyApp({ Component, pageProps }) {
 	const router = useRouter();
 	return (
-		<Layout>
-			<AnimatePresence mode="wait">
-				<motion.div key={router.route} className="h-full">
-					<Transition />
-					<Component {...pageProps} />
-				</motion.div>
-			</AnimatePresence>
-		</Layout>
+		<>
+			<Head>
+				<title>Sudhanshu Bhardwaj</title>
+				<meta property="desciption" content="Sudhanshu's portfolio" />
+			</Head>
+			<Layout>
+				<AnimatePresence mode="wait">
+					<motion.div key={router.route} className="h-full">
+						<Transition />
+						<Component {...pageProps} />
+					</motion.div>
+				</AnimatePresence>
+			</Layout>
+		</>
 	);
 }
 
