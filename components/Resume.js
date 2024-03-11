73c9b1affca5b3eee41c20feb66c2f36ff14711e
Sudhanshu <bsudhanshu3@gmail.com>
2024-03-11 21:44:44 +0530

    minor link change

--- components/Resume.js
+++ components/Resume.js
@@ -2,7 +2,7 @@
 import { BsArrowRight } from 'react-icons/bs';
 
 const RESUME_LINK =
-	'https://drive.google.com/file/d/1sHc7mFymMv_M5f1-OJ1NYy14G0OHwoA3/view?usp=sharing';
+	'https://drive.google.com/file/d/1gJ4k0S8r0Xcgn_921Kmz-gWtX_pCrsOS/view?usp=sharing';
 
 const Resume = () => {
 	return (
